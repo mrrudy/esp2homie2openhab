dde417584e07939fd6c7df118d05ff96faf3cbcb
rudy jam <31594962+mrrudy@users.noreply.github.com>
2018-01-15 23:15:07 +0100

    added automatic upload by means of mqtt

---
+++ tools/platformioUpload.py
@@ -0,0 +1,19 @@
+Import('env')
+from base64 import b64decode
+
+#
+# Dump build environment (for debug)
+# print env.Dump()
+#
+
+env.Replace(UPLOADCMD='/home/rudy/Dokumenty/kod/IOT/esp2homie2openhab/hardware/sonoff_dual/sonoffdualshutters/.piolibdeps/Homie/scripts/ota_updater/ota_updater.py ' + b64decode(ARGUMENTS.get("CUSTOM_OPTION")) + ' "${BUILD_DIR}/${PROGNAME}.bin" ')
+env.Replace(UPLOAD_PORT='3333')
+# uncomment line below to see environment variables
+#print "trying to make line:" + b64decode(ARGUMENTS.get("CUSTOM_OPTION")) + ' ' + map(str, BUILD_DIR) + '/${PROGNAME}.bin'
+print "uploadline: ", env.get("UPLOADCMD")
+
+# print ARGUMENTS
+
+# .piolibdeps/Homie/scripts/ota_updater/ota_updater.py .pioenvs/esp01_1m/firmware.bin -l 192.168.1.25 -t "homie/" -i "5ccf7f9f306c"
+
+#env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", uploadME)
