1c5c3cb86556dbf978c7e436aee3c91108fc39ee
unknown <rudego@gmail.com>
2018-01-15 13:33:13 +0100

    resolving conflict

--- hardware/supla/suplav3shutters/src/main.h
+++ hardware/supla/suplav3shutters/src/main.h
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-#define BOARD_NAME "suplav3"
-#define BOARD_FUTURES "1BLINDS_3BUTTON_OTA"
-#define VERSION "0.93"
-#define DEBUG_PREFIX "DEBUG/"
-#define DEBUG_ENABLED 1
-#define LEFTRELAYPIN 5
-#define RIGHTRELAYPIN 13
-#define BUTTONPIN1 14
-#define BUTTONPIN2 12
-#define BUTTONPIN3 4
-#define LEFT HIGH
-#define RIGHT LOW
-=======
 #define BOARD_NAME "shutters"
 #define BOARD_FUTURES "1SHUTTERS_3BUTTON_OTA"
 #define VERSION "0.93"
->>>>>>> dc18387f5d28b16acbfd8e817b92c8bceef7a5fc
 
 #define DOWNRELAY LEFTRELAYPIN
 #define UPRELAY RIGHTRELAYPIN
