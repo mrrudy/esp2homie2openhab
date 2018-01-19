594db9fc75d41a1c186460630fd06ab7cbdccc46
rudy jam <31594962+mrrudy@users.noreply.github.com>
2018-01-19 23:01:06 +0100

    added switch support, activated OTA upload

--- hardware/sonoff_basic/sonoffbasicswitch/src/main.cpp
+++ hardware/sonoff_basic/sonoffbasicswitch/src/main.cpp
@@ -10,13 +10,21 @@ OneButton button2(J1PIN5, true);
 HomieNode button1Node("b1", "button");
 HomieNode button2Node("b2", "button");
 
+HomieNode relay1("center", "switch");
+
+
+bool relayOnHandler(const HomieRange& range, const String& value) {
+    relaySwitch(RELAYPIN,value=="true" ? HIGH : LOW);
+    relay1.setProperty("on").send(value); //
+    return true;
+}
 
 
 // This function will be called when the button1 was pressed shortly.
 void click1() {
         Debug("button1 click");
         button1Node.setProperty("event").send("click"); //sending this but be sure not to double react on it as below code already enables relay
-relaySwitch(RELAYPIN,true);
+relaySwitch(RELAYPIN,HIGH);
 } // click1
 
 // This function will be called when the button2 was pressed shortly.
@@ -59,6 +67,9 @@ relaySwitch(RELAYPIN,false);
         Homie_setFirmware(BOARD_NAME, VERSION);
         Homie.setup();
 
+        relay1.advertise("on").settable(relayOnHandler);
+
+
         button1Node.setProperty("datatype").send("enum");
         button1Node.setProperty("format").send("click,2click,longclick");
         button1Node.advertise("datatype");
@@ -77,5 +88,6 @@ relaySwitch(RELAYPIN,false);
 void loop() {
 
         Homie.loop();
-
+        button1.tick();
+        button2.tick();
 }
