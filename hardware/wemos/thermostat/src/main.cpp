0ddcfec057faa93785254008ec13b35ee1d58d67
rudy jam <31594962+mrrudy@users.noreply.github.com>
2018-01-26 13:10:27 +0100

    initialized wemoz thermostat project

---
+++ hardware/wemos/thermostat/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Homie.h>
+#include "main.h"
+
+void setup() {
+//        Homie.disableLogging();
+        Homie_setFirmware(BOARD_NAME, VERSION);
+//        Homie.setLedPin(13, HIGH);
+        Homie.setup();
+        Debugf("I'm alive! OTA me pls.");
+        // put your setup code here, to run once:
+}
+
+void loop() {
+        Homie.loop();
+        // put your main code here, to run repeatedly:
+}
---
+++ hardware/wemos/thermostat/src/main.h
@@ -0,0 +1,6 @@
+#define BOARD_NAME "empty"
+#define BOARD_FUTURES "1NOTHING_3NADA_OTA"
+#define VERSION "0.1"
+
+#include "board.h"
+#include "log.h"
