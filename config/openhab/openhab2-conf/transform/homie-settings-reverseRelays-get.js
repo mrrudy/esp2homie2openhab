0a034fefb7a81b1ec65f74d13e897586c85729fe
unknown <31594962+mrrudy@users.noreply.github.com>
2018-12-03 18:14:17 +0100

    openhab config update to reflect new builder items

--- config/openhab/openhab2-conf/transform/homie-settings-reverseRelays-get.js
+++ config/openhab/openhab2-conf/transform/homie-settings-reverseRelays-get.js
@@ -1,6 +1,6 @@
-(function(i) {
+/*(function(i) {
 	var json = JSON.parse(i);
 	return ((json["settings"]["reverseRelays"]) == true ? "ON" : "OFF");
 })(input)
-
-
+*/
+result = ((JSON.parse(input)["settings"]["reverseRelays"]) == true ? "ON" : "OFF");
